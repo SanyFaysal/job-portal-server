937bfa1492924222289da08d546adc85034b3271
Abu Sani Faysal <97028772+SanyFaysal@users.noreply.github.com>
2022-10-21 11:50:42 +0600

    mod : mongoose connection

--- index.js
+++ index.js
@@ -4,12 +4,26 @@ const colors = require('colors');
 
 const app = require('./app');
 
-mongoose.connect(process.env.DATABASE).then(() => {
-  console.log(`Database is connected successfuly`.blue.bold);
-});
+// mongoose.connect(process.env.DATABASE).then(() => {
+//   console.log(`Database is connected successfuly`.blue.bold);
+// });
+
+try {
+  // Connect to the MongoDB cluster
+  mongoose.connect(
+    process.env.DATABASE,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => console.log(` Mongoose is connected`.blue.bold)
+  );
+} catch (e) {
+  console.log(`could not connect`.red.bold);
+}
+
 // server
 const port = process.env.PORT || 8080;
-
+app.get('/', (req, res) => {
+  res.send('hi, this is in live now !!! ');
+});
 app.listen(port, () => {
   console.log(`App is running on port ${port}`.yellow.bold);
 });
