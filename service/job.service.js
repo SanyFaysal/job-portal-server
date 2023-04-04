e0315c6dfc24c6ee3e1e8e2437f3472141aabdc9
SanyFaysal <tfoysalahmed@gmail.com>
2023-04-04 19:37:24 +0600

    mod : get all jobs api

--- service/job.service.js
+++ service/job.service.js
@@ -1,6 +1,6 @@
-const Job = require('../models/Job');
-const mongoose = require('mongoose');
-const User = require('../models/User');
+const Job = require("../models/Job");
+const mongoose = require("mongoose");
+const User = require("../models/User");
 
 exports.createJobService = async (data, hiringManager) => {
   const { _id, name } = hiringManager;
@@ -13,39 +13,49 @@ exports.createJobService = async (data, hiringManager) => {
 };
 exports.getJobByIdService = async (id) => {
   const result = await Job.findOne({ _id: id })
-    .populate('postedBy.id')
-    .populate('applicants')
-    .populate('queries.question.quesBy')
-    .populate('queries.answer.ansBy');
+    .populate("postedBy.id")
+    .populate("applicants")
+    .populate("queries.question.quesBy")
+    .populate("queries.answer.ansBy");
   return result;
 };
 exports.getJobsService = async (sortBy, queries, filter) => {
-
-
+  if (!filter.jobTitle || !filter.jobType || !filter.experience) {
+    const result = await Job.find({})
+      .sort({ createdAt: sortBy })
+      .skip(queries.skip)
+      .limit(queries.limit)
+      .populate("applicants")
+      .populate("postedBy.id");
+    const total = await Job.countDocuments(result);
+    const page = Math.ceil(total / queries.limit);
+    const pageFound = Math.ceil(totalFound / queries.limit);
+    return { page, pageFound, result, total, totalFound: total };
+  }
   const result = await Job.find({
-    "$or": [
-      { "jobTitle": { $regex: filter.jobTitle } },
-      { "jobType": { $regex: filter.jobType } },
-      { "experience": { $regex: filter.experience } }
-    ]
+    $or: [
+      { jobTitle: { $regex: filter.jobTitle } },
+      { jobType: { $regex: filter.jobType } },
+      { experience: { $regex: filter.experience } },
+    ],
   })
     .sort({ createdAt: sortBy })
     .skip(queries.skip)
     .limit(queries.limit)
-    .populate('applicants')
-    .populate('postedBy.id');
+    .populate("applicants")
+    .populate("postedBy.id");
   const total = await Job.countDocuments(result);
   const totalFound = await Job.find({
-    "$or": [
-      { "jobTitle": { $regex: filter.jobTitle } },
-      { "jobType": { $regex: filter.jobType } },
-      { "experience": { $regex: filter.experience } }
-    ]
+    $or: [
+      { jobTitle: { $regex: filter.jobTitle } },
+      { jobType: { $regex: filter.jobType } },
+      { experience: { $regex: filter.experience } },
+    ],
   }).count();
 
   const page = Math.ceil(total / queries.limit);
   const pageFound = Math.ceil(totalFound / queries.limit);
-  console.log({ page, pageFound, result, total, totalFound });
+
   return { page, pageFound, result, total, totalFound };
 
   //   const result = await Job.find({})
@@ -73,30 +83,23 @@ exports.getJobsService = async (sortBy, queries, filter) => {
   //   const page = Math.ceil(total / queries.limit);
   //   return { page, result, total };
   // }
-
 };
 exports.getAllJobsService = async () => {
+  const result = await Job.find({});
 
-
-  const result = await Job.find({})
-
-
-  return result;;
-
-
-
+  return result;
 };
 exports.getManagerJobService = async (employeeId) => {
-  const result = await Job.find({ 'postedBy.id': employeeId }).populate(
-    'applicants'
+  const result = await Job.find({ "postedBy.id": employeeId }).populate(
+    "applicants"
   );
   return result;
 };
 exports.getManagerJobByIdService = async (managerId, jobId) => {
   const result = await Job.find({
     _id: jobId,
-    'postedBy.id': managerId,
-  }).populate('applicants');
+    "postedBy.id": managerId,
+  }).populate("applicants");
   return result;
 };
 exports.updateJobService = async (id, data) => {
@@ -120,7 +123,6 @@ exports.applyJobService = async (jobId, candidateId) => {
   return { updateJob, updateCandidate };
 };
 
-
 exports.createCommentService = async (id, data) => {
   console.log(id, data);
   const result = await Job.updateOne({ _id: id }, { $push: { queries: data } });
@@ -131,9 +133,9 @@ exports.createAnswerService = async (id, data) => {
   const { questionId, ...answer } = data;
   console.log(id, data);
   const result = await Job.updateOne(
-    { _id: id, 'queries._id': questionId }, // filter to find the document and the product to update
-    { $push: { 'queries.$.answer': answer } } // update the claims array for the matching product using the positional operator '$'
-  )
+    { _id: id, "queries._id": questionId }, // filter to find the document and the product to update
+    { $push: { "queries.$.answer": answer } } // update the claims array for the matching product using the positional operator '$'
+  );
   console.log(result);
   return result;
 };
